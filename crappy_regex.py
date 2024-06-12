e301c5a4230e2d0be5b011c2819c0f6f582e2b2a
Sucheta Malladi <sucheta.krsna@gmail.com>
2024-06-12 16:40:28 -0700

    Update crappy_regex.py

--- crappy_regex.py
+++ crappy_regex.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Solution:
     def findPattern(self, p):
         pattern = []
@@ -10,18 +12,35 @@ class Solution:
                 pattern.append(p[i])
         return pattern
 
-    def checkMatch(self, currs, nexts, p):
-        truth_table = ([False]*len(s))*len(p)
-        
-        for match in pattern:
-            if "*" in match:
-                currp = match.split(*)
-            
+    def make_tt(self, s, pattern):
+        tt = np.full((len(pattern), len(s)), False)
+        for match in range(len(pattern)):
+            for char in range(len(s)):
+                print(char, match)
+                print(pattern[match])
+                print(s[char])
+                if s[char] == pattern[match].replace("*",""):
+                    tt[match][char] = True
+                print(tt[match][char])
+        return tt
+    
+    def traverse_tt(self, tt, pattern):
+        num_row, num_col = np.shape(tt)
+        for row in range(num_row):
+            for col in range(num_col):
+                if tt[row][col] == True:
+                    
 
+            
     def isMatch(self, s: str, p: str) -> bool:
         pattern = self.findPattern(p)
-
-        for i in range(len(s)):
-            checkMatch(self, s[i], s[i+1], p)
-            
+        tt = self.make_tt(s, pattern)
+        print(tt)
         return True
+    
+def main():
+    sol = Solution()
+    sol.isMatch("aabba", "a*ab*a")
+
+if __name__ == "__main__":
+        main()
